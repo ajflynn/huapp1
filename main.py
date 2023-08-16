6316a165ba976d203e8fd8f22e5e8649388870e5
ajflynn <ajflynn@umich.edu>
2023-08-16 12:29:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.title("Welcome to Health Universe!")
+st.title("Medication name analyzer.")
 st.write("This is a sample application.")
 
-alarm_clock = st.slider('hour', 0, 23, 17) # min: 0h, max: 23h, default: 17h
-st.header(alarm_clock) # print in large text
+user_input = st.text_input("Medication name")
+
+st.write(user_input)
